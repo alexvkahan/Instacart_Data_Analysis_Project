8fe48b72416626c39ab090120c0fbf37eb455ffe
Chris Muhlenkamp <56003993+camuhl@users.noreply.github.com>
2021-09-08 15:16:17 -0700

    updating progress from session on 9/8/21

--- orders.sql
+++ orders.sql
@@ -4,6 +4,7 @@ select * from orders
 fetch first 10 rows only;
 
 --Create lookup table for day of week to convert numberic value to text
+-- look into doing it with the small plsql block found here https://stackoverflow.com/questions/39576/best-way-to-do-multi-row-insert-in-oracle
 create table dow_lookup
 (
     dow_id number primary key,
@@ -31,13 +32,63 @@ create table dow_lookup
  Insert into dow_lookup (dow_id, day_of_week) 
  values (6, 'Saturday');
 
- 
- 
 
+ 
+--.423, .389
 select 
-    o.order_dow,
+    dl.day_of_week,
     o.order_hour_of_day,
     count(*) num_orders
 from orders o
-group by o.order_dow, o.order_hour_of_day
-order by num_orders desc;
+join dow_lookup dl on o.order_dow = dl.dow_id
+group by rollup(dl.day_of_week, o.order_hour_of_day)
+order by
+     CASE
+          WHEN dl.day_of_week = 'Sunday' THEN 1
+          WHEN dl.day_of_week = 'Monday' THEN 2
+          WHEN dl.day_of_week = 'Tuesday' THEN 3
+          WHEN dl.day_of_week = 'Wednesday' THEN 4
+          WHEN dl.day_of_week = 'Thursday' THEN 5
+          WHEN dl.day_of_week = 'Friday' THEN 6
+          WHEN dl.day_of_week = 'Saturday' THEN 7
+     END ASC,
+     o.order_hour_of_day;
+
+--.403, .391
+select 
+    a.day_of_week,
+    a.order_hour_of_day,
+    a.num_orders
+from (
+    select 
+        dl.dow_id,
+        dl.day_of_week,
+        o.order_hour_of_day,
+        count(*) num_orders
+    from orders o
+    join dow_lookup dl on o.order_dow = dl.dow_id
+    group by dl.dow_id, dl.day_of_week, o.order_hour_of_day
+    order by dl.dow_id, o.order_hour_of_day
+) a;
+
+--.413, .382
+--roll up gives strange results, when inside the subquery will repeat, when outside switches the order by back to alphebetical for day_of_week
+with a as (
+    select 
+        dl.dow_id,
+        dl.day_of_week,
+        o.order_hour_of_day,
+        count(*) num_orders
+    from orders o
+    join dow_lookup dl on o.order_dow = dl.dow_id
+    group by  dl.dow_id, dl.day_of_week, o.order_hour_of_day
+    order by dl.dow_id, o.order_hour_of_day
+    )
+select 
+    a.day_of_week,
+    a.order_hour_of_day,
+    max(a.num_orders)
+from a
+group by rollup(a.day_of_week, a.order_hour_of_day);
+
+
